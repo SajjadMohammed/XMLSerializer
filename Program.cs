0efb8276f2846909626d77882b76ebaf71d97d37
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2017-11-09 21:17:53 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defining an object
+            var student = new Student
+            {
+                Id = 1,
+                FullName = "Ahmed Ameen Ali",
+                Address = "Kirkuk",
+                Age = 23
+            };
+            XMLSerialize(student);
+            var stu = XMLDeserialize("xmlfile.xml");
+            Console.WriteLine($"{stu.Id} : {stu.FullName} : {stu.Address} : {stu.Age}");
+            Console.ReadKey();
+        }
+
+        static void XMLSerialize(Student student)
+        {
+            using (var xmlFile = File.Create("xmlfile.xml"))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(Student));
+                xmlSerializer.Serialize(xmlFile, student);
+            }
+        }
+
+        static Student XMLDeserialize(string filePath)
+        {
+            using (var xmlFile = File.OpenRead(filePath))
+            {
+                var xmlDeserializer = new XmlSerializer(typeof(Student));
+                var student = xmlDeserializer.Deserialize(xmlFile) as Student;
+                return student;
+            }
+        }
+    }
+}
